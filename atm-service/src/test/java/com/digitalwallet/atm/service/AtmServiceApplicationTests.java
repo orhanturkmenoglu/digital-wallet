a2805b96c187b562f429678af13d15e38fca719d
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2025-04-13 22:56:08 +0300

    Initial commit – Setup digitalWallet microservices architecture

---
+++ atm-service/src/test/java/com/digitalwallet/atm/service/AtmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalwallet.atm.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
