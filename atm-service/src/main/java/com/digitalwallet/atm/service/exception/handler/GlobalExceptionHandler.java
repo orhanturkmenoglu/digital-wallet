d8d7e6ca8d788dc4e64a6172525d3ea49be7e94c
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2025-05-07 10:04:35 +0300

    feat(atm-service): add GlobalExceptionHandler to handle ATM, Address, and Location related exceptions with custom error responses
    .log to project

--- atm-service/src/main/java/com/digitalwallet/atm/service/exception/handler/GlobalExceptionHandler.java
+++ atm-service/src/main/java/com/digitalwallet/atm/service/exception/handler/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.digitalwallet.atm.service.exception.handler;
 
-import com.digitalwallet.atm.service.exception.ATMNotFoundException;
-import com.digitalwallet.atm.service.exception.ApiErrorResponse;
-import com.digitalwallet.atm.service.exception.ATMInvalidParametersException;
+import com.digitalwallet.atm.service.exception.*;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,55 +17,24 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ApiErrorResponse> handleValidationException(ValidationException validationException){
-       ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
-               .timeStamp(LocalDateTime.now())
-               .status(HttpStatus.BAD_REQUEST.value())
-               .errorCode(validationException.getMessage())
-               .message("VALIDATION_ERROR")
-               .build();
-
-       return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(ATMInvalidParametersException.class)
-    public ResponseEntity<ApiErrorResponse> handleInvalidATMParametersException(ATMInvalidParametersException invalidATMParametersException,
-                                                                                WebRequest request){
-
+    public ResponseEntity<ApiErrorResponse> handleValidationException(ValidationException validationException) {
         ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
                 .timeStamp(LocalDateTime.now())
                 .status(HttpStatus.BAD_REQUEST.value())
-                .errorCode(invalidATMParametersException.getMessage())
-                .message("INVALID_ATM_PARAMETERS")
-                .path(request.getDescription(false))
+                .errorCode(validationException.getMessage())
+                .message("VALIDATION_ERROR")
                 .build();
 
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ATMNotFoundException.class)
-    public ResponseEntity<ApiErrorResponse> handleATMNotFoundException(ATMNotFoundException atmNotFoundException,
-                                                                       WebRequest request){
-
-        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
-                .timeStamp(LocalDateTime.now())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .errorCode(atmNotFoundException.getMessage())
-                .message("ATM_NOT_FOUND")
-                .path(request.getDescription(false))
-                .build();
-
-        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-
-    @ExceptionHandler
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> handleValidationErrors(MethodArgumentNotValidException exception,
-                                                                   WebRequest request){
+                                                                   WebRequest request) {
         BindingResult bindingResult = exception.getBindingResult();
         StringBuilder message = new StringBuilder("Validation failed: ");
 
-        for (FieldError error:bindingResult.getFieldErrors()){
+        for (FieldError error : bindingResult.getFieldErrors()) {
             message.append(error.getField())
                     .append("-")
                     .append(error.getRejectedValue())
@@ -85,4 +52,129 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ATMInvalidParametersException.class)
+    public ResponseEntity<ApiErrorResponse> handleInvalidATMParametersException(ATMInvalidParametersException invalidATMParametersException,
+                                                                                WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .errorCode(invalidATMParametersException.getMessage())
+                .message("INVALID_ATM_PARAMETERS")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ATMNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleATMNotFoundException(ATMNotFoundException atmNotFoundException,
+                                                                       WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .errorCode(atmNotFoundException.getMessage())
+                .message("ATM_NOT_FOUND")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(ATMServiceCodeInvalidParameterException.class)
+    public ResponseEntity<ApiErrorResponse> handleATMServiceCodeInvalidParameterException(ATMServiceCodeInvalidParameterException invalidParameterException,
+                                                                       WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .errorCode(invalidParameterException.getMessage())
+                .message("INVALID_ATM_SERVICE_CODE")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ATMServiceCodeNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleATMServiceCodeNotFoundException(ATMServiceCodeNotFoundException atmServiceCodeNotFoundException,
+                                                                       WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .errorCode(atmServiceCodeNotFoundException.getMessage())
+                .message("ATM_SERVICE_CODE_NOT_FOUND")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(AddressInvalidParameterException.class)
+    public ResponseEntity<ApiErrorResponse> handleAddressInvalidParameterException(AddressInvalidParameterException invalidParameterException,
+                                                                                   WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .errorCode(invalidParameterException.getMessage())
+                .message("ATM_SERVICE_CODE_NOT_FOUND")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AddressNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleAddressNotFoundException(AddressNotFoundException addressNotFoundException,
+                                                                           WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .errorCode(addressNotFoundException.getMessage())
+                .message("ATM_SERVICE_CODE_NOT_FOUND")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(LocationInvalidParameterException.class)
+    public ResponseEntity<ApiErrorResponse> handleLocationInvalidParameterException(LocationInvalidParameterException invalidParameterException,
+                                                                                    WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .errorCode(invalidParameterException.getMessage())
+                .message("LOCATION_INVALID_PARAMETER")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(LocationNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleLocationNotFoundException(LocationNotFoundException locationNotFoundException,
+                                                                            WebRequest request) {
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .errorCode(locationNotFoundException.getMessage())
+                .message("LOCATION_NOT_FOUND")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
 }
